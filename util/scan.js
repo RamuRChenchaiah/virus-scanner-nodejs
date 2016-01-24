25d48c5df1d9e78bef4e36529267d03092e50c9e
Ramu Chenchaiah <rchencha@cisco.com>
2016-01-24 19:22:43 +0530

    Introducing logger & few cleanups

--- util/scan.js
+++ util/scan.js
@@ -1,3 +1,5 @@
+var logger = require('winston')
+
 var clam = require('clamscan')({
     remove_infected: true, // Removes files if they are infected 
     quarantine_infected: '~/infected/', // Move file here. remove_infected must be FALSE, though. 
@@ -6,31 +8,32 @@ var clam = require('clamscan')({
     debug_mode: true, // This will put some debug info in your js console 
     file_list: '/home/webuser/scan_files.txt', // path to file containing list of files to scan 
     clamscan: {
-        path: '/usr/bin/clam', // I dunno, maybe your clamscan is just call "clam" 
+        path: '/usr/bin/clam',
         db: '/usr/bin/better_clam_db', // Path to a custom virus definition database 
-        scan_archives: false, // Choosing false here will save some CPU cycles 
-        active: false // you don't want to use this at all because it's evil 
+        scan_archives: false,
+        active: false 
     },
     clamdscan: {
         path: '/bin/clamdscan', // Special path to the clamdscan binary on your server 
         config_file: __dirname + '/logs/clamscan-log', // logs file in your app directory 
-        multiscan: false, // You hate speed and multi-threaded awesome-sauce 
-        reload_db: true, // You want your scans to run slow like with clamscan 
-        active: false // you don't want to use this at all because it's evil 
+        multiscan: true,
+        reload_db: true,
+        active: false 
     },
-    preference: 'clamscan' // If clamscan is found and active, it will be used by default    
+    preference: 'clamscan'    
 });
 
-
+// scans a test file from the location: /var/picture/
 clam.is_infected('/var/picture/for_example.jpg', function(err, file, is_infected) {
     if(err) {
-        console.log(err);
+        logger.error(err);
         return false;
-    }
- 
+    } 
     if(is_infected) {
+		logger.info("File is infected!");
         res.send({msg: "File is infected!"});
     } else {
+		logger.info("File is clean!");
         res.send({msg: "File is clean!"});
     }
 });
